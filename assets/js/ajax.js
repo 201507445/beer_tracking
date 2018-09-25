dfdc2800b213a116fceb9e86a95dcaf04f074c99
Sihle Socishe <sihlesocishe@gmail.com>
2018-09-25 11:03:04 +0200

    Ajax update

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,41 +1,22 @@
 $(document).ready(function () {
+    var toSearch = {};
     $.getJSON("http://apichallenge.canpango.com/categories/", function (cat) {
         for (var i = 0; i < cat.length; i++) {
-            $('#cat').append('<li><a href="' + cat[i] + '">' + cat[i].name + '</a></li>');
+            //console.log(cat[i].url);
+            toSearch.url = cat[i].url;
+            toSearch.name = cat[i].name;
+            $('#cat').append('<li><a href="' + cat[i].url + '">' + cat[i].name + '</a></li>');
         }
+        console.log(toSearch);
     });
-    /*$(".beer-review-link").on("click", function (e) {
-
-        $('#myModal').modal('show');
-
-    });*/
-    $('#searchField').keyup(function () {
-        var input, filter, ul, li, a, i, count = 0;
-        input = $(this);
-        filter = input.val().toUpperCase();
-        container = $(".beer-container");
-        beer = container.find('div.beer');
-        if (beer.length !== 0) {
-            // Loop through all list items, and hide those who don't match the search query
-
-            for (i = 0; i < beer.length; i++) {
-                a = beer[i].getElementsByClassName("__l")[0];
-                if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                    beer[i].style.display = "";
-                    count++;
-                } else {
-                    beer[i].style.display = "none";
-                }
-            }
-            if (count === 0) {
-                if (container.find('._cLno_r').length === 0) {
-                    container.prepend('<div class="row _cLno_r">' +
-                            '<ul><li class="no-info">No results</li></ul></div>');
-                }
-            } else {
-                container.find('._cLno_r').remove();
+    $.getJSON("http://apichallenge.canpango.com/beers", function (beer) {
+        for (var i = 0; i < beer.length; i++) {
+            for (var key in beer[i]) {
+                //if (beer[i][key].indexOf(toSearch) !== -1) {
+                  //s  results.push(beer[i]);
+               // }
             }
         }
-
     });
+
 });
