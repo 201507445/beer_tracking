0f6904bee2c14285911c9a26af65147e1b957b11
201507445 <sihlesocishe@gmail.com>
2018-09-25 13:22:18 +0200

    Ajax stach

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -3,3 +3,17 @@ $(".beer-review-link").on("click", function(){
     
     
 });
+$(function(){
+	$('#addBeer-form').submit(function(event){
+		event.preventDefault();
+		$.ajax({
+			url: '/beers/search/',
+			data: $(this).serialize(),
+			dataType:'json',
+			success: function(){
+				
+			}
+		});
+
+	});
+});
